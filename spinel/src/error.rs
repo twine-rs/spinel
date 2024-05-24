39ec4a5b3fd139a676b2c1db23be584af5ce6e4a
Jake Swensen <jake@swensen.io>
2024-05-24 07:10:13 -0700

    feat: error: add more error types

--- spinel/src/error.rs
+++ spinel/src/error.rs
@@ -1,3 +1,19 @@
+use crate::{Frame, Status};
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "std")] {
+        type IoError = String;
+
+        impl From<std::io::Error> for Error {
+            fn from(e: std::io::Error) -> Self {
+                Error::Io(e.to_string())
+            }
+        }
+    } else {
+        type IoError = ();
+    }
+}
+
 #[derive(Debug, PartialEq)]
 #[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
 pub enum Error {
@@ -10,8 +26,20 @@ pub enum Error {
     HdlcStartDelimiter(u8),
     #[cfg_attr(feature = "thiserror", error("Incorrect ending delimiter: {0}"))]
     HdlcEndDelimiter(u8),
+    #[cfg_attr(
+        feature = "thiserror",
+        error("Could not send message, host connection failure")
+    )]
+    HostConnectionSend,
+    #[cfg_attr(
+        feature = "thiserror",
+        error("Could not receive message, host connection failure")
+    )]
+    HostConnectionRecv,
     #[cfg_attr(feature = "thiserror", error("Unknown command: {0}"))]
     Command(u32),
+    #[cfg_attr(feature = "thiserror", error("IO Error: {0}"))]
+    Io(IoError),
     #[cfg_attr(feature = "thiserror", error("Unknown property: {0}"))]
     Property(u32),
     #[cfg_attr(
@@ -21,4 +49,10 @@ pub enum Error {
     PackedU32ByteCount,
     #[cfg_attr(feature = "thiserror", error("Incorrect packet length: {0}"))]
     PacketLength(usize),
+    #[cfg_attr(feature = "thiserror", error("Error configuring serial port"))]
+    SerialConfig,
+    #[cfg_attr(feature = "thiserror", error("Target status: {0}"))]
+    Status(Status),
+    #[cfg_attr(feature = "thiserror", error("Target sent unexpected response: {0}"))]
+    UnexpectedResponse(Frame),
 }
